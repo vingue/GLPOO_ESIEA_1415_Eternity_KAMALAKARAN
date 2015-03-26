9e70610251cbc80d11449ef6bb10cbc2a93638d6
Nes <Nes@Nesrine>
2015-03-26 02:33:35 -0700

    Test pour adapter ColorinFrench

--- src/test/java/fr/esiea/glpoo/ColorTest.java
+++ src/test/java/fr/esiea/glpoo/ColorTest.java
@@ -2,12 +2,15 @@ package fr.esiea.glpoo;
 
 import java.awt.Color;
 
+import org.junit.Test;
+
+import fr.ybonnel.csvengine.validator.ValidateException;
 import junit.framework.Assert;
 
 public class ColorTest {
 
 	@Test
-	public void testColorConversion() {
+	public void testColorConversion() throws ValidateException {
 		String s = "bleu";
 		Color c = new Colorinfrench().parse(s);
 		Assert.assertSame(c, Color.blue);;
