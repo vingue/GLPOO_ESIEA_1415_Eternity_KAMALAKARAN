43a580d4f9713c64da00d439ac0ddce8e589d8bc
vingue <guerin.vincentalex@gmail.com>
2015-03-26 09:51:15 +0100

    fenetre

---
+++ src/main/java/fr/esiea/glpoo/Main.java
@@ -0,0 +1,21 @@
+package fr.esiea.glpoo;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Main {
+    
+    public Main() {
+        JFrame frame=new JFrame("Eternity");
+        frame.setMinimumSize(new Dimension(640,480));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new Main();
+    }
+    
+}
