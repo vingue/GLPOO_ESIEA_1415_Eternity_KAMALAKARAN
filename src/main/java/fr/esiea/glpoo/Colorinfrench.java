e7b80805a6e3b4192be6283020cd566ba8192ad3
Nes <Nes@Nesrine>
2015-05-01 18:20:45 +0200

    J'ai modif color in french ça m'affiche bien la couleur du csv maintenant faut que la couleur s'affiche dans la frame

--- src/main/java/fr/esiea/glpoo/Colorinfrench.java
+++ src/main/java/fr/esiea/glpoo/Colorinfrench.java
@@ -1,21 +1,15 @@
 package fr.esiea.glpoo;
 
 import java.awt.Color;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-import fr.ybonnel.csvengine.adapter.AdapterCsv;
-import fr.ybonnel.csvengine.annotation.CsvFile;
-import fr.ybonnel.csvengine.validator.ValidateException;
 
 
-public class Colorinfrench extends AdapterCsv<Color>{
+
+
+public class Colorinfrench{
+	
 	
 
-@Override
-public Color parse(String str) throws ValidateException {
+public Color parse(String str) {
 	Color color = null;
 	switch(str){
 	case "bleu":
@@ -37,7 +31,6 @@ public Color parse(String str) throws ValidateException {
 	return color;
 }
 
-@Override
 public String toString(Color color) {
 	// TODO Auto-generated method stub
 	return null;
--- src/main/java/fr/esiea/glpoo/Main.java
+++ src/main/java/fr/esiea/glpoo/Main.java
@@ -4,18 +4,38 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Main {
-    
-    public Main() {
-        JFrame frame=new JFrame("Eternity");
+/**
+ * Hello world!
+ *
+ */
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Main extends Pieces
+{
+
+	Pieces piece = new Pieces();
+	String color=piece.run();
+	
+	Color test=new Colorinfrench().parse(color);
+
+	
+	
+	public Main() {
+       
+		JFrame frame=new JFrame("Eternity");
         frame.setMinimumSize(new Dimension(640,480));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(test);
         frame.pack();
         frame.setVisible(true);
+        
+        
     }
     
     public static void main(String[] args) {
         new Main();
     }
-    
 }
