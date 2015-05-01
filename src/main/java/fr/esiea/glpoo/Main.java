ab6f849cfb50d5abc8c87647f07ef5daf9e650ca
vingue <guerin.vincentalex@gmail.com>
2015-05-01 18:43:03 +0200

    nouveau test et fenetre améliorée

--- src/main/java/fr/esiea/glpoo/Main.java
+++ src/main/java/fr/esiea/glpoo/Main.java
@@ -1,15 +1,38 @@
 package fr.esiea.glpoo;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
     
+	Color couleur=Color.cyan;
+	
     public Main() {
-        JFrame frame=new JFrame("Eternity");
+        JFrame frame=new JFrame("Eternity II");
         frame.setMinimumSize(new Dimension(640,480));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(couleur);
+        
+        JPanel buttonPanel=new JPanel();
+        JPanel aidePanel=new JPanel();
+        JButton resetButton = new JButton("Restart");
+        JButton undoButton = new JButton("Undo");
+        JButton rotateButton = new JButton("Rotate");
+        buttonPanel.add(resetButton);
+        buttonPanel.add(undoButton);
+        buttonPanel.add(rotateButton);
+        aidePanel.setLayout(new BorderLayout());
+        aidePanel.setBorder(BorderFactory.createTitledBorder("Aide"));
+        aidePanel.add(buttonPanel, BorderLayout.EAST);
+        frame.add(aidePanel);
+        
         frame.pack();
         frame.setVisible(true);
     }
