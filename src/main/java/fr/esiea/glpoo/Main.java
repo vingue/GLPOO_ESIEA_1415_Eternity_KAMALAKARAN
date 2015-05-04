7166b7f1fb8a4551033be5546947157e22739143
vingue <guerin.vincentalex@gmail.com>
2015-05-04 17:34:19 +0200

    New

--- src/main/java/fr/esiea/glpoo/Main.java
+++ src/main/java/fr/esiea/glpoo/Main.java
@@ -4,38 +4,80 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Main {
+public class Main implements ActionListener{
     
 	Color couleur=Color.cyan;
+	private int val=0;
+	JLabel text= new JLabel();
+
 	
     public Main() {
-        JFrame frame=new JFrame("On est les + mieux!");
+        JFrame frame=new JFrame("Eternity II");
 
 
         frame.setMinimumSize(new Dimension(640,480));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(couleur);
         
+        frame.setLayout(new BorderLayout());
         JPanel buttonPanel=new JPanel();
         JPanel aidePanel=new JPanel();
+        JPanel piecePanel=new JPanel();
         JButton resetButton = new JButton("Restart");
+        resetButton.setActionCommand("reset");
+        resetButton.addActionListener(this);
         JButton undoButton = new JButton("Undo");
+        undoButton.setActionCommand("undo");
+        undoButton.addActionListener(this);
         JButton rotateButton = new JButton("Rotate");
-        JButton rotateButtonn = new JButton("VincentOnAReussi");
-        buttonPanel.add(resetButton);
+        rotateButton.setActionCommand("rotate");
+        rotateButton.addActionListener(this);
+        JButton redoButton = new JButton("Redo");
+        redoButton.setActionCommand("redo");
+        redoButton.addActionListener(this);
+        File file=new File("./image/Googolopoly.jpg");
+        JLabel lab = new JLabel(new ImageIcon(file.getPath())); 
+        File file2=new File("./image/piece1.jpg");
+        JLabel lab2 = new JLabel(new ImageIcon(file2.getPath()));
+        File file3=new File("./image/piece2.png");
+        JLabel lab3 = new JLabel(new ImageIcon(file3.getPath()));
+        File file4=new File("./image/piece3.png");
+        JLabel lab4 = new JLabel(new ImageIcon(file4.getPath()));
+        File file5=new File("./image/piece4.png");
+        JLabel lab5 = new JLabel(new ImageIcon(file5.getPath()));
+        text.setText("Valeur de départ: "+val);
+                
+        buttonPanel.setLayout(new GridLayout(2,2));
         buttonPanel.add(undoButton);
+        buttonPanel.add(redoButton);
         buttonPanel.add(rotateButton);
-        buttonPanel.add(rotateButtonn);
+        buttonPanel.add(resetButton);
+        buttonPanel.setBorder(BorderFactory.createTitledBorder("Boutons"));
+        piecePanel.setLayout(new GridLayout(2,2));
+        piecePanel.add(lab2);
+        piecePanel.add(lab3);
+        piecePanel.add(lab4);
+        piecePanel.add(lab5);
+        piecePanel.setBorder(BorderFactory.createTitledBorder("Pieces"));
         aidePanel.setLayout(new BorderLayout());
         aidePanel.setBorder(BorderFactory.createTitledBorder("Aide"));
-        aidePanel.add(buttonPanel, BorderLayout.EAST);
-        frame.add(aidePanel);
+        aidePanel.add(buttonPanel, BorderLayout.NORTH);
+        aidePanel.add(text,BorderLayout.CENTER);
+        aidePanel.add(piecePanel, BorderLayout.SOUTH);
+        frame.add(aidePanel, BorderLayout.EAST);
+        frame.add(lab, BorderLayout.WEST);
         
         frame.pack();
        
@@ -45,5 +87,31 @@ public class Main {
     public static void main(String[] args) {
         new Main();
     }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getActionCommand()) {
+			case "reset":
+				val=0;
+				text.setText("La valeur a été réinitialisé à "+val);
+				break;
+
+			case "undo":
+				val--;
+				text.setText("On a rétiré 1 à notre valeur on a donc: "+val);
+				break;
+
+			case "redo":
+				val++;
+				text.setText("On a ajouté 1 à notre valeur on a donc: "+val);
+				break;
+				
+			case "rotate":
+				val*=-1;
+				text.setText("On a inversé notre valeur on a donc: "+val);
+				break;
+		}
+	}
     
 }
