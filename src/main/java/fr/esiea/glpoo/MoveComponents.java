0dc58552d68aaf12ea8d76e38e839df5b2eb35a3
Benjichoun <batista@et.esiea.fr>
2015-05-10 21:53:39 +0200

    Petit changement pour le deplacement

--- src/main/java/fr/esiea/glpoo/MoveComponents.java
+++ src/main/java/fr/esiea/glpoo/MoveComponents.java
@@ -5,8 +5,12 @@ import java.awt.Container;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
  
+
+
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
  
 public class MoveComponents extends JPanel {
@@ -21,9 +25,9 @@ public class MoveComponents extends JPanel {
         setLayout(null); // on supprime le layout manager
  
         ComponentMove listener = new ComponentMove(this);
-        for(int i=0; i<10; i++) {
+       // for(int i=0; i<10; i++) {
             add(createComponent());
-        }
+      //  }
         addMouseListener(listener);
         addMouseMotionListener(listener);
  
@@ -32,10 +36,15 @@ public class MoveComponents extends JPanel {
     private final static Color[] COLORS= {Color.RED, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.PINK, Color.WHITE, Color.BLACK};
  
     private JComponent createComponent() {
-        JPanel component=new JPanel(); // ici on peut faire n'importe quel JComponent, JLabel, par exemple
-        component.setLocation(500,300 ); // position aléatoire
-        component.setSize(10+(int)(Math.random()*100), 10+(int)(Math.random()*100)); // taille aléatoire
-        component.setBackground(COLORS[(int)(Math.random()*COLORS.length)]); // couleur aléatoire
+        JLabel component=new JLabel(new ImageIcon("src/main/ressources/piece3.png"));
+        
+       
+       // ici on peut faire n'importe quel JComponent, JLabel, par exemple
+        
+        component.setLocation(500,300 ); // position vers l'endroit des pieces
+      //  component.setSize(10+(int)(Math.random()*100), 10+(int)(Math.random()*100)); // taille aléatoire
+        component.setSize(200,200);
+        //component.setBackground(COLORS[(int)(Math.random()*COLORS.length)]); // couleur aléatoire
         component.setEnabled(false); // les composants ne doivent pas intercepter la souris
         return component;
     }
